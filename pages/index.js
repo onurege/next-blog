a34bc0a62beb600df01b291186a12bb4d5c466c1
onurege <9784793@ogrenci.bilecik.edu.tr>
2021-04-30 19:20:12 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function HomePage() {
-    return <div className="text-purple-500 bg-blue-100">Welcome to Next.js!</div>
+    return <div className="max">
+      <h1>Ben Onur Ege Usluer. Bilgisayar Mühendisliği 3.sınıf öğrencisiyim. </h1>
+      <p>Front-end teknolojileri ve powerlifting sporuyla yakından ilgileniyorum.</p>
+      <p>
+        Bu projede, sektördeki yeni nesil teknolojileri, github linkim üzerinden açık kaynak bir şekilde paylaşmayı amaçlıyorum.
+      </p>
+    </div>
   }
   
   export default HomePage
